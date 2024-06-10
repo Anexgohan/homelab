6a9a9d774a606e06b4f4418253c12455dba6f099
Anexgohan <monish05m@gmail.com>
2024-06-11 03:51:57 +0530

    powershell added
    tested

---
+++ configs/terminal/oh-my-posh/terminals/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+# ---------- local ---------- #
+# oh-my-posh init pwsh --config 'E:\git\repositories\homelab\configs\terminal\oh-my-posh\profiles\json\.anex_oh-my-posh_theme_01.json' | Invoke-Expression
+
+# ---------- Github ---------- #
+# .original-default.json
+# oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/profiles/json/.original-default.json' | Invoke-Expression
+
+# .anex_oh-my-posh_theme_01.json
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/profiles/json/.anex_oh-my-posh_theme_01.json' | Invoke-Expression
