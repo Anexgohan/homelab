b9a813e31804f2eb1b1e5c1cb20726042c283517
Anexgohan <monish05m@gmail.com>
2024-06-13 03:31:45 +0530

    test

--- scripts/oh-my-post-install.sh
+++ scripts/oh-my-post-install.sh
@@ -30,13 +30,43 @@ cd $DIR_PATH
 # install dependencies:
 apt-get install -y curl wget unzip
 # download oh-my-posh anex custom script:
+curl -s https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/scripts/linux/install.sh -o $DIR_PATH/install.sh
+bash $DIR_PATH/install.sh
 # curl -s https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/scripts/linux/install.sh | bash -s
-curl -s https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/scripts/linux/install.sh | bash -s -- -d $DIR_PATH
+# curl -s https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/scripts/linux/install.sh | bash -s -- -d $DIR_PATH
 
 echo '-----'
 echo "For more information, visit: https://www.nerdfonts.com/font-downloads"
 echo "Recommended Fonts: FiraCode or Meslo LGM NF"
 echo '-----'
+echo "Do you want to install fonts? (y/n)"
+read install_fonts
+# convert to lowercase:
+install_fonts=$(echo $install_fonts | tr '[:upper:]' '[:lower:]')
+if [ "$install_fonts" = "y" ] || [ "$install_fonts" = "yes" ]; then
+    # present a choice of fonts to install:
+    echo "Please select a font to install:"
+    echo "Recommended: FiraCode or Meslo LGM NF"
+    options=("FiraCode" "MonoFont" "Select manually")
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "FiraCode")
+                oh-my-posh font install FiraCode
+                break
+                ;;
+            "MonoFont")
+                oh-my-posh font install Meslo
+                break
+                ;;
+            "Select manually")
+                oh-my-posh font install
+                break
+                ;;
+            *) echo "invalid option $REPLY";;
+        esac
+    done
+fi
 
 # download profile "anex.omp.json" using wget to DIR_PATH/profiles:
 wget -N -P $DIR_PATH/profiles 'https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/profiles/json/anex.omp.json'
