3e7a078e2cace237bf93c1223e9118aea82a1bea
Anexgohan <monish05m@gmail.com>
2024-06-13 05:05:21 +0530

    release

--- scripts/oh-my-post-install.sh
+++ scripts/oh-my-post-install.sh
@@ -31,22 +31,29 @@ cd $DIR_PATH
 apt-get install -y curl wget unzip
 # download oh-my-posh anex custom script:
 curl -s https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/scripts/linux/install.sh -o $DIR_PATH/install.sh
-bash $DIR_PATH/install.sh
+sh $DIR_PATH/install.sh
 # curl -s https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/scripts/linux/install.sh | bash -s
 # curl -s https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/scripts/linux/install.sh | bash -s -- -d $DIR_PATH
 
 echo '-----'
-echo "For more information, visit: https://www.nerdfonts.com/font-downloads"
-echo "Recommended Fonts: FiraCode or Meslo LGM NF"
+echo -e "For more information, visit:" "\e[36m"https://www.nerdfonts.com/font-downloads"\e[0m"
+echo -e "Recommended Fonts:" "\e[32m""FiraCode or Meslo LGM NF""\e[0m"
 echo '-----'
-echo "Do you want to install fonts? (y/n)"
+
+# echo "Please select a nerd font to install:"
+# echo "Recommended: FiraCode or Meslo LGM NF"
+# echo "for more information, visit: https://www.nerdfonts.com/font-downloads"
+# FiraCode,             oh-my-posh font install FiraCode
+# Meslo LGM NF          oh-my-posh font install Meslo
+# Select manually       oh-my-posh font install
+
+echo -e "\e[32m""Do you want to install fonts? (y/n)""\e[0m"
 read install_fonts
-# convert to lowercase:
 install_fonts=$(echo $install_fonts | tr '[:upper:]' '[:lower:]')
 if [ "$install_fonts" = "y" ] || [ "$install_fonts" = "yes" ]; then
     # present a choice of fonts to install:
     echo "Please select a font to install:"
-    echo "Recommended: FiraCode or Meslo LGM NF"
+    echo # Recommended: FiraCode or Meslo LGM NF
     options=("FiraCode" "MonoFont" "Select manually")
     select opt in "${options[@]}"
     do
@@ -68,13 +75,16 @@ if [ "$install_fonts" = "y" ] || [ "$install_fonts" = "yes" ]; then
     done
 fi
 
-# download profile "anex.omp.json" using wget to DIR_PATH/profiles:
-wget -N -P $DIR_PATH/profiles 'https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/profiles/json/anex.omp.json'
+# download "anex.omp.json" profile using wget:
+wget -N -P "$DIR_PATH/profiles" 'https://raw.githubusercontent.com/Anexgohan/homelab/main/configs/terminal/oh-my-posh/profiles/json/anex.omp.json'
 
-# edit .bashrc to include oh-my-posh initialization edit if not already present:
-if ! grep -Fxq "eval \"$(oh-my-posh init bash --config $DIR_PATH/profiles/anex.omp.json)\"" /root/.bashrc
+# add oh-my-posh profile to ".bashrc" if not already present:
+# oh-my-posh
+# eval "$(oh-my-posh init bash --config $DIR_PATH/profiles/anex.omp.json)"
+if ! grep -Fxq 'eval "$(oh-my-posh init bash --config '"$DIR_PATH"'/profiles/anex.omp.json)"' /root/.bashrc
 then
-    echo -e "\n\n# oh-my-posh\neval \"$(oh-my-posh init bash --config $DIR_PATH/profiles/anex.omp.json)\"" | tee -a /root/.bashrc
+    echo -e "\n\n# oh-my-posh" '\neval "$(oh-my-posh init bash --config '"$DIR_PATH"'/profiles/anex.omp.json)"' | tee -a /root/.bashrc
 fi
 
+# reload your profile for the changes to take effect:
 exec bash
